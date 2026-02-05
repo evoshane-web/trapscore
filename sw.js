2c0eabcb78c9dd2621306075353307a903954aef
evoshane-web <evoshane@gmail.com>
2026-02-04 22:09:35 -0500

    Update service worker to use new cache version and assets

--- sw.js
+++ sw.js
@@ -1,10 +1,22 @@
-const CACHE_NAME = 'trapscore-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'trapscore-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.tailwindcss.com'
+];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
